ab8a5db372fd305278d71bd7e29cc52385cf3be9
log <log@doople.net>
2020-03-03 18:32:20 +0900

    입출력2

--- src/Main2441.java
+++ src/Main2441.java
@@ -3,7 +3,17 @@ import java.util.Scanner;
 public class Main2441 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        int count = sc.nextInt();
+        sc.close();
+        for (int i = 1; i <=count; i++) {
+            for (int j = 1; j <=i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = i; k <=count; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
         
     }
 
