47f332779cfb6493a41e36d8c1e903559990ab47
log <log@doople.net>
2020-03-06 17:54:53 +0900

    부분수열 완료

--- src/dp/Main11053.java
+++ src/dp/Main11053.java
@@ -10,19 +10,26 @@ public class Main11053 {
         int dp[] = new int[n + 1];
         for (int i = 1; i <= n; i++) {
             num[i] = sc.nextInt();
-        }
-        dp[1] = 1;
-
-        for (int i = 1; i <= n; i++) {
             dp[i] = 1;
+        }
+
+        /*for (int i = 2; i <=n ; i++) {
             for (int j = 1; j <= i; j++) {
-                System.out.println("---- " + i);
-                if (num[j] < num[i] && dp[j] >= dp[i]) {
+                if(num[i] > num[i-j] ){
+                    dp[i] = Math.max(dp[i], dp[j] +1);
+                }
+            }
+        }*/
+
+        for (int i = 1; i <=n ; i++) {
+            for (int j = 1; j <=i; j++) {
+                if(num[j] < num[i] && dp[j] >=dp[i] ){
                     dp[i] = dp[j] + 1;
                 }
-                System.out.println("dp[i]  " + dp[i]);
             }
         }
+
+
         int max =0;
         for(int i=1; i<=n;i++){
             if(dp[i] > max){
--- src/dp/Main11054.java
+++ src/dp/Main11054.java
@@ -5,8 +5,36 @@ import java.util.Scanner;
 public class Main11054 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int num[] = new int[n + 1];
+        int dph[] = new int[n + 1];
+        int dpl[] = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            num[i] = sc.nextInt();
+            dph[i] = 1;
+            dpl[i] = 1;
+        }
 
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                if (num[j] < num[i] && dph[j] >= dph[i]) {
+                    dph[i] = dph[j] + 1;
+                }
+            }
+        }
+
+        for (int i = n - 1; i >= 1; i--) {
+            for (int j = i; j <= n; j++) {
+                if (num[j] < num[i] && dpl[j] >= dpl[i]) {
+                    dpl[i] = dpl[j] + 1;
+                }
+            }
+        }
+
+        int max = 0;
+        for (int i = 1; i <= n; i++) {
+            if (dph[i]+ dpl[i] > max) max =dph[i]+ dpl[i];
+        }
+        System.out.println(max-1);
     }
-
-
 }
